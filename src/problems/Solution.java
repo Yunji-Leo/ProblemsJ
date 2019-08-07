9115b249e1f60a37555c35b9a15fd5b619aceb51
Leo Zhong <yunji@uber.com>
2019-08-06 23:51:08 -0700

    Swap Nodes in Pairs

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -735,4 +735,19 @@ public class Solution {
         return dummy.next;
     }
 
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        prev.next = head;
+        while (prev.next!=null&&prev.next.next!=null){
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+            prev = first;
+        }
+        return dummy.next;
+    }
+
 }
