53150d92bf9537ae05700bfb165a94fd0d3c9130
Leo Zhong <yunji@uber.com>
2019-09-04 10:23:38 -0700

    longest valid parentheses

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -906,7 +906,7 @@ public class Solution {
 
         int j = i;
         for (; j < nums.length; j++) {
-            if (j == nums.length - 1 || ( nums[j + 1] <= nums[i])) {  //notice <=
+            if (j == nums.length - 1 || (nums[j + 1] <= nums[i])) {  //notice <=
                 break;
             }
         }
@@ -927,6 +927,22 @@ public class Solution {
             i++;
             j--;
         }
+    }
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
     }
 
 }
