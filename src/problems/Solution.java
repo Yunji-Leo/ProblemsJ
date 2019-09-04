dccff14ba56741f80c446c6b168d5556d4a12b72
Leo Zhong <yunji@uber.com>
2019-09-04 10:31:55 -0700

    lvp stack

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -945,4 +945,22 @@ public class Solution {
         return maxans;
     }
 
+    public int longestValidParenthesesStack(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
 }
