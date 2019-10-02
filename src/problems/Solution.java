ca6fd4ff9904224ee5b5524aa1c470aeba5b34b0
Leo Zhong <yunji@uber.com>
2019-10-01 22:29:03 -0700

    rotate search 2

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -1021,4 +1021,28 @@ public class Solution {
         }
         return -1;
     }
+
+    public int search2(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] < nums[end]) {
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            } else {
+                if (target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
