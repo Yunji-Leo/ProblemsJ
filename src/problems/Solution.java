d927e7fb5f53b88cebe705f37a2067c8551a8264
Leo Zhong <yunji@uber.com>
2019-10-01 22:47:07 -0700

    combination sum

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -1070,4 +1070,24 @@ public class Solution {
         }
         return curr.toString();
     }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSumBacktrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+
+    private void combinationSumBacktrack(List<List<Integer>> list, List<Integer> templist, int[] nums, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) {
+            list.add(new ArrayList<>(templist));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                templist.add(nums[i]);
+                combinationSumBacktrack(list, templist, nums, remain - nums[i], i);
+                templist.remove(templist.size() - 1);
+            }
+        }
+    }
 }
