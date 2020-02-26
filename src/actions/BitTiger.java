e825e63827a6c5c1db3f4946097df76e3f07d56d
Leo Zhong <yunji@uber.com>
2020-02-25 16:59:07 -0800

    i change to j is not always equals j change to i

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -310,4 +310,40 @@ public class BitTiger {
         ListNode rightList = mergeKListsDC(lists, mid + 1, right);
         return mergeTwoLists(leftList, rightList);
     }
+
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+
+    public int minDistance(String word1, String word2) {
+        if (word1 == null && word2 == null) {
+            return 0;
+        }
+        if (word1 == "") {
+            return word2.length();
+        }
+        if (word2 == "") {
+            return word1.length();
+        }
+
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= word2.length(); j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
 }
