160e97c9583912c2bd28d86f338a3f02ba5e9baa
Leo Zhong <yunji@uber.com>
2020-02-26 20:47:02 -0800

    four direction {0,1,0,-1} {1,0,-1,0} %4

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -497,6 +497,46 @@ public class BitTiger {
         return new ArrayList<>(map.values());
     }
 
+    public int maxSubArray(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+            result = Math.max(result, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix == null || matrix.length == 0) {
+            return ans;
+        }
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]) {
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
+    }
+
     public int minDistance(String word1, String word2) {
         if (word1 == null && word2 == null) {
             return 0;
