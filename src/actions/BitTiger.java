ca6637a1d15acfd99aa08d3677fc8ae8f4b46336
Leo Zhong <yunji@uber.com>
2020-02-26 21:35:52 -0800

    Comparator, Collections.sort, Arrays.asList, extends the end time

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -554,6 +554,27 @@ public class BitTiger {
         return true;
     }
 
+    public int[][] merge(int[][] intervals) {
+        Collections.sort(Arrays.asList(intervals), new IntervalComparator());
+
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    private class IntervalComparator implements Comparator<int[]> {
+        public int compare(int[] a, int[] b) {
+            return a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1;
+        }
+    }
+
     public int minDistance(String word1, String word2) {
         if (word1 == null && word2 == null) {
             return 0;
