4f65fde198599037b5ede54f749af688880b3dde
Leo Zhong <yunji@uber.com>
2020-02-28 12:02:36 -0800

    binary recursive

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -1088,5 +1088,25 @@ public class BitTiger {
         return node;
     }
 
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        return sortedArrayToBSTRecur(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBSTRecur(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBSTRecur(nums, left, mid - 1);
+        node.right = sortedArrayToBSTRecur(nums, mid + 1, right);
+        return node;
+    }
+
 
 }
