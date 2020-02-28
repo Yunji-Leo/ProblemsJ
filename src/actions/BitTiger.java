2583e4501667f2d2bffdc550694c0fbc968673a4
Leo Zhong <yunji@uber.com>
2020-02-28 10:51:56 -0800

    no need using memo, no overlap

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -1053,4 +1053,13 @@ public class BitTiger {
         }
         return result;
     }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+
 }
