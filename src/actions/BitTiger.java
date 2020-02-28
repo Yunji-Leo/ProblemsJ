6fd6f87ff3d15035ab4cfb4ad1a9ddf2fc8d76a9
Leo Zhong <yunji@uber.com>
2020-02-28 12:18:36 -0800

    one by one compare

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -1144,5 +1144,15 @@ public class BitTiger {
         return result;
     }
 
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
+
 
 }
