f5a6dead7d9acf714b039d5fe0540cf5c8ee9209
Leo Zhong <yunji@uber.com>
2020-02-29 13:25:29 -0800

    count 5

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -164,4 +164,13 @@ public class BitTiger2 {
         }
         return result;
     }
+
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (n / 5 != 0) {
+            result += n / 5;
+            n /= 5;
+        }
+        return result;
+    }
 }
