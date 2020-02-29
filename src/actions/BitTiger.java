7d609db8f374e878895fb7f9f1bfce7263d7e5e1
Leo Zhong <yunji@uber.com>
2020-02-28 21:06:53 -0800

    no full learned, keep the latest largest/smallest multiplier

--- src/actions/BitTiger.java
+++ src/actions/BitTiger.java
@@ -1488,4 +1488,22 @@ public class BitTiger {
         }
         return stack.pop();
     }
+
+    public int maxProduct(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        int result = max;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int tmp = min;
+                min = max;
+                max = tmp;
+            }
+            max = Math.max(nums[i], nums[i] * max);
+            min = Math.min(nums[i], nums[i] * min);
+            result = Math.max(result, max);
+        }
+
+        return result;
+    }
 }
