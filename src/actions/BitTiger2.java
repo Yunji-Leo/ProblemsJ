50d3a02bd3909dc676f01934e4f6f8976ab8dbc0
Leo Zhong <yunji@uber.com>
2020-02-28 23:02:38 -0800

    binary search the peak

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -66,4 +66,16 @@ public class BitTiger2 {
             runnerB = runnerB.next;
         }
     }
+
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
 }
