3d1d771ec1e4ce7878b7b0a7f250da4a1c38af4d
Leo Zhong <yunji@uber.com>
2020-03-01 15:02:33 -0800

    reverse link list

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -342,4 +342,15 @@ public class BitTiger2 {
         return count;
     }
 
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
 }
