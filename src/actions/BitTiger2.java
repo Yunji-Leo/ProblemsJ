0e0527bc6787587ecbf6a79825cc344883c28f7d
Leo Zhong <yunji@uber.com>
2020-02-29 19:34:36 -0800

    at least have 1 blood

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -173,4 +173,28 @@ public class BitTiger2 {
         }
         return result;
     }
+
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon == null || dungeon.length == 0) {
+            return 0;
+        }
+        int M = dungeon.length;
+        int N = dungeon[0].length;
+        int[][] dp = new int[M][N];
+        dp[M - 1][N - 1] = Math.max(1, 1 - dungeon[M - 1][N - 1]);
+        for (int i = M - 2; i >= 0; i--) {
+            dp[i][N - 1] = Math.max(1, dp[i + 1][N - 1] - dungeon[i][N - 1]);
+        }
+        for (int j = N - 2; j >= 0; j--) {
+            dp[M - 1][j] = Math.max(1, dp[M - 1][j + 1] - dungeon[M - 1][j]);
+        }
+
+        for (int i = M - 2; i >= 0; i--) {
+            for (int j = N - 2; j >= 0; j--) {
+                dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+            }
+        }
+
+        return dp[0][0];
+    }
 }
