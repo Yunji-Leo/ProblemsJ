f7467f54034b0a790dfc6b0e11ada3c586b2c664
Leo Zhong <yunji@uber.com>
2020-03-01 16:25:46 -0800

    break circle into two lists, need take of the index of dp[]

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -496,5 +496,34 @@ public class BitTiger2 {
         return result;
     }
 
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return nums[0] > nums[1] ? nums[0] : nums[1];
+        }
+        if (nums.length == 3) {
+            return Math.max(nums[0], Math.max(nums[1], nums[2]));
+        }
+
+        int[] dp = new int[nums.length - 1];
+        dp[0] = nums[0];
+        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+        for (int i = 2; i < nums.length - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        int result = dp[dp.length - 1];
+        dp[0] = nums[1];
+        dp[1] = nums[1] > nums[2] ? nums[1] : nums[2];
+        for (int i = 3; i < nums.length; i++) {
+            dp[i - 1] = Math.max(dp[i - 1 - 1], dp[i - 1 - 2] + nums[i]);
+        }
+        return Math.max(result, dp[dp.length - 1]);
+    }
+
 
 }
