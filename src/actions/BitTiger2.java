a8d8526ad6c988d97eca21c93e50dd4de884a4e5
Leo Zhong <yunji@uber.com>
2020-03-03 13:11:32 -0800

    (1) elements smaller than the 'median' are put into the last even slots
    (2) elements larger than the 'median' are put into the first odd slots

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -1065,4 +1065,28 @@ public class BitTiger2 {
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 
+    public void wiggleSort(int[] nums) {
+        int median = findKthLargest(nums, (nums.length + 1) / 2);
+        int n = nums.length;
+
+        int left = 0, i = 0, right = n - 1;
+
+        while (i <= right) {
+
+            if (nums[newIndex(i, n)] > median) {
+                swap(nums, newIndex(left++, n), newIndex(i++, n));
+            } else if (nums[newIndex(i, n)] < median) {
+                swap(nums, newIndex(right--, n), newIndex(i, n));
+            } else {
+                i++;
+            }
+        }
+
+
+    }
+
+    private int newIndex(int index, int n) {
+        return (1 + 2 * index) % (n | 1);
+    }
+
 }
