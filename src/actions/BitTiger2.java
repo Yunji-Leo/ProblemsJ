40cf4e6c894ffc80b9fc1511f62c382179938004
Leo Zhong <yunji@uber.com>
2020-03-03 17:26:28 -0800

    easy

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -1186,4 +1186,19 @@ public class BitTiger2 {
         }
     }
 
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
 }
