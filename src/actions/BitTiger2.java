a0b403d36b6c9f7733378cc036528a4833d0357d
Leo Zhong <yunji@uber.com>
2020-03-04 14:59:44 -0800

    hashmap to count each char

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -1414,4 +1414,17 @@ public class BitTiger2 {
         }
     }
 
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
