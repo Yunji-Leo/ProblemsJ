e14cb7a6984c0e0c03b62a7f43f52cec90011e98
Leo Zhong <yunji@uber.com>
2020-03-04 21:08:25 -0800

    hashmap to log sum count

--- src/actions/BitTiger2.java
+++ src/actions/BitTiger2.java
@@ -1574,4 +1574,20 @@ public class BitTiger2 {
         return result;
     }
 
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int a : A) {
+            for (int b : B) {
+                map.put(a + b, map.getOrDefault(a + b, 0) + 1);
+            }
+        }
+        int result = 0;
+        for (int c : C) {
+            for (int d : D) {
+                result += map.getOrDefault(-c - d, 0);
+            }
+        }
+        return result;
+    }
+
 }
