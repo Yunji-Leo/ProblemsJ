50ea8e76721c32ecdb1fc7dc74fb242d0405e1b1
Leo Zhong <yunji@uber.com>
2020-03-05 23:41:00 -0800

    Trap! the left bound is different from right bound

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -125,4 +125,49 @@ public class TopLiked {
         }
         return maxans;
     }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return new int[]{-1, -1};
+        }
+
+        int[] result = new int[2];
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (nums[left] == target) {
+            result[0] = left;
+        } else {
+            return new int[]{-1, -1};
+        }
+
+        left = 0;
+        right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = (left + right) / 2;
+            if (nums[mid] <= target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (nums[right] == target) {
+            result[1] = right;
+        } else {
+            result[1] = left;
+        }
+
+
+        return result;
+    }
 }
