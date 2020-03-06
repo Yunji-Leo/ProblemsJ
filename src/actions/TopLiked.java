7c2e09ab36358bc321766d8c2fcc3b06a6689471
Leo Zhong <yunji@uber.com>
2020-03-05 21:52:43 -0800

    dp top down, take care of edge check

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -15,11 +15,11 @@ public class TopLiked {
     private boolean isMatchTopDown(String s, int i, String p, int j, int[][] dp) {
         if (i == s.length()) {
             while (j < p.length()) {
-                if (s.charAt(j) != '*') {
-                    return true;
+                if (p.charAt(j) != '*') {
+                    return false;
                 }
             }
-            return false;
+            return true;
         }
         if (j == p.length()) {
             return false;
@@ -42,6 +42,47 @@ public class TopLiked {
             dp[i][j] = -1;
         }
 
+        return result;
+    }
+
+    public boolean isMatch2(String s, String p) {
+        int[][] dp = new int[s.length()][p.length()];
+        return isMatch2TopDown(s, 0, p, 0, dp);
+    }
+
+    private boolean isMatch2TopDown(String s, int i, String p, int j, int[][] dp) {
+        if (i == s.length()) {
+            while (j < p.length()) {
+                if (j == p.length() - 1 || p.charAt(j + 1) != '*') {
+                    return false;
+                }
+                j += 2;
+            }
+            return true;
+        }
+        if (j == p.length()) {
+            return false;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j] == 1;
+        }
+
+        boolean result = false;
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                result = isMatch2TopDown(s, i + 1, p, j, dp) || isMatch2TopDown(s, i, p, j + 2, dp);
+            } else {
+                result = isMatch2TopDown(s, i + 1, p, j + 1, dp);
+            }
+        } else if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+            result = isMatch2TopDown(s, i, p, j + 2, dp);
+        }
+
+        if (result) {
+            dp[i][j] = 1;
+        } else {
+            dp[i][j] = -1;
+        }
         return result;
     }
 }
