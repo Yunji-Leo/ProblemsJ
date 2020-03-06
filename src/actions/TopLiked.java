667a8a333ad0f5e31a5812ff6ad29e136cfecfea
Leo Zhong <yunji@uber.com>
2020-03-06 13:42:29 -0800

    subset needs index, but permutation does not

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -290,5 +290,21 @@ public class TopLiked {
         nums[j] = tmp;
     }
 
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        subsetsBacktrack(nums, 0, result, new ArrayList<>());
+        return result;
+    }
+
+    private void subsetsBacktrack(int[] nums, int index, List<List<Integer>> result, List<Integer> temp) {
+        result.add(new ArrayList<>(temp));
+
+        for (int i = index; i < nums.length; i++) {
+            temp.add(nums[i]);
+            subsetsBacktrack(nums, index + 1, result, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
 
 }
