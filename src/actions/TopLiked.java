4f954a77c299a79f3f73195e62be22f4ff5abc5f
Leo Zhong <yunji@uber.com>
2020-03-06 20:43:20 -0800

    dp for i-1,j i,j-1, i-1,j-1

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -530,4 +530,37 @@ public class TopLiked {
         return l.next;
     }
 
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+                result = 1;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == '1') {
+                dp[0][j] = 1;
+                result = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+        }
+        return result * result;
+    }
+
 }
