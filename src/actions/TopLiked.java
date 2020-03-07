3b3af9ad4529c05ee1e064f011473abe3ee5d970
Leo Zhong <yunji@uber.com>
2020-03-06 18:49:29 -0800

    Trap, can not put fast==slow in while condition

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -468,4 +468,28 @@ public class TopLiked {
         return false;
     }
 
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
 }
