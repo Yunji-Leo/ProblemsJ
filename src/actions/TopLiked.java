ec82909492fefceb638c0fbfbb7d90965768e560
Leo Zhong <yunji@uber.com>
2020-03-06 20:47:36 -0800

    use queue for level travel

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -563,4 +563,15 @@ public class TopLiked {
         return result * result;
     }
 
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
 }
