ca8468ec0eeb7699306442e0c88c02a528eeb296
Leo Zhong <yunji@uber.com>
2020-03-07 22:18:58 -0800

    from largest to insert smaller

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -721,7 +721,33 @@ public class TopLiked {
     }
 
     public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0)
+            return new int[0][0];
 
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o2[0] - o1[0];
+            }
+        });
+
+        int n = people.length;
+        ArrayList<int[]> tmp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            tmp.add(people[i][1], new int[]{people[i][0], people[i][1]});
+        }
+
+        int[][] res = new int[people.length][2];
+        int i = 0;
+        for (int[] k : tmp) {
+            res[i][0] = k[0];
+            res[i][1] = k[1];
+            i++;
+        }
+        return res;
     }
 
 }
