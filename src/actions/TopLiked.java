7893839bb854855f91d32298d72dd328eb1c3256
Leo Zhong <yunji@uber.com>
2020-03-09 16:38:28 -0700

    recursive i/2+i%2

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -885,4 +885,11 @@ public class TopLiked {
         return res;
     }
 
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i = 1; i <= num; i++)
+            f[i] = f[i / 2] + i % 2;
+        return f;
+    }
+
 }
