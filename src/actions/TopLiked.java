6c56e44428e867be7ed4c2c6bd46f5161fb91922
Leo Zhong <yunji@uber.com>
2020-03-09 23:28:40 -0700

    similar 0/1 backpack

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -952,4 +952,28 @@ public class TopLiked {
         }
         return ret;
     }
+
+    public int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row : memo)
+            Arrays.fill(row, Integer.MIN_VALUE);
+        return calculate(nums, 0, 0, S, memo);
+    }
+
+    private int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (memo[i][sum + 1000] != Integer.MIN_VALUE) {
+                return memo[i][sum + 1000];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + 1000] = add + subtract;
+            return memo[i][sum + 1000];
+        }
+    }
 }
