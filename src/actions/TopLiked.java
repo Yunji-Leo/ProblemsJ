212e19adfcba128400803927f2b32c1216209a09
Leo Zhong <yunji@uber.com>
2020-03-10 11:49:26 -0700

    merge tree value

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -1029,4 +1029,17 @@ public class TopLiked {
         }
         return r - l > 0 ? r - l + 1 : 0;
     }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        TreeNode left = mergeTrees(t1.left, t2.left);
+        TreeNode right = mergeTrees(t1.right, t2.right);
+        t1.left = left;
+        t1.right = right;
+        return t1;
+    }
 }
