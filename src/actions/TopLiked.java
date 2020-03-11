66a1ddc5db918e9a6c27a9db6e8980137540c769
Leo Zhong <yunji@uber.com>
2020-03-10 21:46:27 -0700

    brilliant greedy

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -1099,4 +1099,16 @@ public class TopLiked {
         }
         return greedy[nums.length - 1] == Integer.MAX_VALUE ? 0 : greedy[nums.length - 1];
     }
+
+    public int jumpGreedy(int[] nums) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
 }
