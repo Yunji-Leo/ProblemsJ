a3fef8705adf1e5a1bf1027926b297532f0a01fe
Leo Zhong <yunji@uber.com>
2020-03-11 12:25:30 -0700

    smaller substract larger add

--- src/actions/TopInterviewed.java
+++ src/actions/TopInterviewed.java
@@ -16,4 +16,68 @@ public class TopInterviewed {
 
         return result;
     }
+
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        int sign = 1, base = 0, i = 0;
+        while (i < str.length() && str.charAt(i) == ' ') {
+            i++;
+        }
+        if (i == str.length())
+            return 0;
+        if (str.charAt(i) == '-' || str.charAt(i) == '+') {
+            if (str.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if (base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
+                if (sign == 1) return Integer.MAX_VALUE;
+                else return Integer.MIN_VALUE;
+            }
+            base = 10 * base + (str.charAt(i) - '0');
+            i++;
+        }
+        return base * sign;
+    }
+
+    public int romanToInt(String s) {
+        int nums[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'M':
+                    nums[i] = 1000;
+                    break;
+                case 'D':
+                    nums[i] = 500;
+                    break;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+                case 'L':
+                    nums[i] = 50;
+                    break;
+                case 'X':
+                    nums[i] = 10;
+                    break;
+                case 'V':
+                    nums[i] = 5;
+                    break;
+                case 'I':
+                    nums[i] = 1;
+                    break;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1])
+                sum -= nums[i];
+            else
+                sum += nums[i];
+        }
+        return sum + nums[nums.length - 1];
+    }
 }
