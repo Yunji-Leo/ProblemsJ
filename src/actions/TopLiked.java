46694a5df6787c9493f20106c291178dbfe6f14f
Leo Zhong <yunji@uber.com>
2020-03-10 21:30:31 -0700

    expand at center

--- src/actions/TopLiked.java
+++ src/actions/TopLiked.java
@@ -1054,4 +1054,22 @@ public class TopLiked {
         }
         return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
     }
+
+    public int countSubstrings(String s) {
+        int N = s.length(), ans = 0;
+        for (int center = 0; center <= 2 * N - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < N && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+
+    public int[] dailyTemperatures(int[] T) {
+        
+    }
 }
