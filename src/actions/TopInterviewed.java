dc2169e2a273f6e3ae5aa739b09ea3f8c5e551e2
Leo Zhong <yunji@uber.com>
2020-03-11 20:17:19 -0700

    int to long, divide by 2

--- src/actions/TopInterviewed.java
+++ src/actions/TopInterviewed.java
@@ -201,4 +201,21 @@ public class TopInterviewed {
         }
         return dp[0][0];
     }
+
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        double ans = 1;
+        while (n > 0) {
+            if (n % 2 == 1)
+                ans *= x;
+            x *= x;
+            n /= 2;
+        }
+        return ans;
+    }
 }
